7c476e812065f0e604fcc354def03c436472fd55
Lautaro-Agazzi <lagazzi@mail.austral.edu.ar>
2025-08-11 12:34:13 -0300

    Add files via upload

--- exercise_math.py
+++ exercise_math.py
@@ -1,3 +1,17 @@
 def math():
     a = 57
     b = 7
+    print(a + b)    # Suma
+    resta = a - b   # Resta
+    print(resta)    
+    producto = a * b   #Producto
+    print(producto)    
+    promedio = (a + b) /2 # Promedio
+    print(promedio)    
+    cociente_enteros = a // b  # Cociente entero
+    print(cociente_enteros)    
+    resto_division = a % b   # Resto de la división
+    print(resto_division)    
+    valor_real = a / b   # Valor real
+    print(valor_real)    
+math()
